21d0ad8aab053a5f158a88e536af69bdf5da5e9e
Juliano Lorenzato <julianolorenzato@gmail.com>
2024-02-03 14:59:07 -0300

    gitignore

---
+++ src/arg_parser/arg_parser.h
@@ -0,0 +1,3 @@
+#include <stdbool.h>
+
+void parse(char const *argv[], int *nsets, int *bsize, int *assoc, char *r_policy, bool output_flag);
--- src/main.c
+++ src/main.c
@@ -1,8 +1,16 @@
 #include <stdio.h>
+#include <stdbool.h>
+#include "arg_parser/arg_parser.h"
 
 int main(int argc, char const *argv[])
 {
-    printf("Hello World!\n");
+    int nsets, bsize, assoc;
+    char r_policy;
+    bool output_flag;
+
+    parse(argv, &nsets, &bsize, &assoc, &r_policy, &output_flag);
+
+    printf("%d %d %d %c %d\n\n", nsets, bsize, assoc, r_policy, output_flag);
 
     return 0;
 }
